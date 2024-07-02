f4f8f1909b025669569daafd7ac9e064ab0c2a4f
jayakumar <jk416848@gmail.com>
2024-07-02 20:10:36 +0530

    initial commit 5

--- Movie_blog/settings.py
+++ Movie_blog/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [STATIC_DIRS]
-STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStorages'
 MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
